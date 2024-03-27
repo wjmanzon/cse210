cea1a98cf5da007a2f253d79baae3673ab5e85c0
wjmanzon <wilsonjr.manzon@gmail.com>
2024-03-26 19:56:53 -0600

    transferred files to correct folder

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        // Create a list of videos
+        List<Video> videos = new List<Video>
+        {
+            new Video("How to Program in C#", "CoderA", 600),
+            new Video("Understanding Abstraction", "EducatorB", 300),
+            new Video("Principles of OOP", "InstructorC", 450)
+        };
+
+        // Add comments to each video
+        videos[0].AddComment(new Comment("Alejandro", "Great tutorial!"));
+        videos[0].AddComment(new Comment("Leah", "Very helpful, thanks!"));
+        videos[1].AddComment(new Comment("Audrey", "This cleared up a lot for me."));
+        videos[2].AddComment(new Comment("Ben", "Can't wait for the next video!"));
+        videos[2].AddComment(new Comment("Olivia", "This makes so much sense now."));
+
+        // Display video information and comments
+        foreach (Video video in videos)
+        {
+            Console.WriteLine($"Title: {video.Title}, Author: {video.Author}, Length: {video.LengthInSeconds} seconds");
+            Console.WriteLine($"Number of Comments: {video.GetNumberOfComments()}");
+            foreach (Comment comment in video.GetComments())
+            {
+                Console.WriteLine($"- {comment.Name}: {comment.Text}");
+            }
+            Console.WriteLine(); // Add an empty line for better readability
+        }
     }
 }
