b8ab8015baa9836ae9844c83bf7813bb8fed9f18
wjmanzon <wilsonjr.manzon@gmail.com>
2024-03-26 20:05:15 -0600

    fixed typo

--- final/Foundation1/Comment.cs
+++ final/Foundation1/Comment.cs
@@ -1,7 +1,7 @@
 public class Comment
 {
     public string Name { get; set;} // Commenter's name
-    public string Test { get; set; } // Comment text
+    public string Text { get; set; } // Comment text
 
     public Comment(string name, string text)
     {
